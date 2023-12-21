00947cd356a28c242ac08666234160732f65f0f3
yarikkot04 <yarikkotenkoim13@gmail.com>
2023-12-21 23:09:19 +0200

    Removed protection from /csrf route for correct work with the postman.

--- routes/csrf.js
+++ routes/csrf.js
@@ -1,8 +1,7 @@
 const { Router } = require('express')
 const router = Router()
-const auth = require('../middlewares/auth')
 
-router.get('/', auth, async (req, res) => {
+router.get('/', async (req, res) => {
     const csrf = req.csrfToken()
     res.json(csrf)
 })
